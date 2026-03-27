be5af9a1e59140c1774e337069f46d0d4eb4f63d
Anand kumar <anandkumarsaidi2005@gmail.com>
2026-03-27 22:50:49 +0530

    removed contributing.md

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -131,7 +131,6 @@ const login = async (req : Request, res : Response)=>{
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            // sameSite: "strict",
             sameSite: "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
