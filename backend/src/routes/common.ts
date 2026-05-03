44198c0809f53400b84acbfa5f7ad0aa1fda4e3d
Anand kumar <anandkumarsaidi2005@gmail.com>
2026-05-03 12:39:21 +0530

    feat: added alumni profile public

--- backend/src/routes/common.ts
+++ backend/src/routes/common.ts
@@ -1,6 +1,6 @@
 
 import express, { Router } from "express"
-import { findAllAlumni } from "../controller/userController.js";
+import { findAllAlumni, findUser } from "../controller/userController.js";
 import { exportUsers } from "../controller/adminLogController.js";
 
 const router: Router = express.Router();
@@ -8,5 +8,7 @@ const router: Router = express.Router();
 router.get("/alumnis", findAllAlumni); // permits to all without signup or signin
 
 router.get("/exports", exportUsers); // This must should be removed after testing
+
+router.get("/profile/:id", findUser);
 
 export default router;
